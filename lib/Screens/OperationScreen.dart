633b8ab282fb6b65bf1dd09c0ab373fc0ea1594a
Shaurya Thakur <176936155+Shaurya0987@users.noreply.github.com>
2026-01-21 21:43:42 +0530

    Form and Model Added

--- lib/Screens/OperationScreen.dart
+++ lib/Screens/OperationScreen.dart
@@ -1,5 +1,6 @@
 import 'package:firebaselogindemo/Firebase/StudentDataBase.dart';
 import 'package:firebaselogindemo/Models/Student_Model.dart';
+import 'package:firebaselogindemo/Screens/ProfileGate.dart';
 import 'package:flutter/material.dart';
 
 class Operationscreen extends StatefulWidget {
@@ -24,133 +25,133 @@ class _OperationscreenState extends State<Operationscreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(centerTitle: true, title: Text("Operation Screen")),
-      body: Padding(
-        padding: const EdgeInsets.all(28.0),
-        child: Center(
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10),
-                  child: TextFormField(
-                    controller: nameController,
-                    decoration: InputDecoration(
-                      label: Text("Enter Name"),
-                      border: OutlineInputBorder(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(28.0),
+          child: Center(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: TextFormField(
+                      controller: nameController,
+                      decoration: InputDecoration(
+                        label: Text("Enter Name"),
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Enter Name";
+                        }
+                        return null;
+                      },
                     ),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return "Enter Name";
-                      }
-                      return null;
-                    },
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10),
-                  child: TextFormField(
-                    controller: collegeController,
-                    decoration: InputDecoration(
-                      label: Text("Enter collge nme"),
-                      border: OutlineInputBorder(),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: TextFormField(
+                      controller: collegeController,
+                      decoration: InputDecoration(
+                        label: Text("Enter collge nme"),
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Enter College Name";
+                        }
+                        return null;
+                      },
                     ),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return "Enter College Name";
-                      }
-                      return null;
-                    },
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10),
-                  child: TextFormField(
-                    controller: branchController,
-                    decoration: InputDecoration(
-                      label: Text("Enter Branch Name"),
-                      border: OutlineInputBorder(),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: TextFormField(
+                      controller: branchController,
+                      decoration: InputDecoration(
+                        label: Text("Enter Branch Name"),
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Enter Branch Name";
+                        }
+                        return null;
+                      },
                     ),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return "Enter Branch Name";
-                      }
-                      return null;
-                    },
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10),
-                  child: TextFormField(
-                    controller: semesterController,
-                    decoration: InputDecoration(
-                      label: Text("Enter  Current Semester"),
-                      border: OutlineInputBorder(),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: TextFormField(
+                      controller: semesterController,
+                      decoration: InputDecoration(
+                        label: Text("Enter  Current Semester"),
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Enter Semester";
+                        }
+                        return null;
+                      },
                     ),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return "Enter Semester";
-                      }
-                      return null;
-                    },
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 10),
-                  child: TextFormField(
-                    controller: rollNoController,
-                    decoration: InputDecoration(
-                      label: Text("Enter  RollNo"),
-                      border: OutlineInputBorder(),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: TextFormField(
+                      controller: rollNoController,
+                      decoration: InputDecoration(
+                        label: Text("Enter  RollNo"),
+                        border: OutlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return "Enter Roll No";
+                        }
+                        return null;
+                      },
                     ),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return "Enter Roll No";
-                      }
-                      return null;
-                    },
                   ),
-                ),
-            
-                ElevatedButton(
-                  onPressed: () async{
-                    if(_formKey.currentState!.validate()){
-                      final student = StudentModel(
-                      name: nameController.text.trim(),
-                      college: collegeController.text.trim(),
-                      semester: int.parse(semesterController.text.trim()),
-                      branch: branchController.text.trim(),
-                      rollNo: int.parse(rollNoController.text.trim()),
-                    );
-                    service.createStudent("ucbs", rollNoController.text, student);
+              
+                  ElevatedButton(
+                    onPressed: () async{
+                      if(_formKey.currentState!.validate()){
+                        final student = StudentModel(
+                        name: nameController.text.trim(),
+                        college: collegeController.text.trim(),
+                        semester: int.parse(semesterController.text.trim()),
+                        branch: branchController.text.trim(),
+                        rollNo: int.parse(rollNoController.text.trim()),
+                      );
+                      service.createStudent("ucbs", rollNoController.text, student);
+                      }
+                    },
+                    child: Text("Create Student"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      if(_formKey.currentState!.validate()){
+                      service.updateStudent('ucbs', rollNoController.text, {
+                        "name": "Sherry",
+                        'branch': 'BBA',
+                      });
                     }
-                  },
-                  child: Text("Create Student"),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    if(_formKey.currentState!.validate()){
-                    service.updateStudent('ucbs', rollNoController.text, {
-                      "name": "Sherry",
-                      'branch': 'BBA',
-                    });
-                  }
-                  },
-                  child: Text("Update Student"),
-                ),
-                ElevatedButton(onPressed: () {
-                  if(_formKey.currentState!.validate()){
-                    
-                  }
-                }, child: Text("Read Student")),
-                ElevatedButton(
-                  onPressed: () {
-                    service.deleteStudent("ucbs", rollNoController.text);
-                  },
-                  child: Text("Delete Student"),
-                ),
-              ],
+                    },
+                    child: Text("Update Student"),
+                  ),
+                  ElevatedButton(onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=>ProfileScreen()));
+                  }, child: Text("Read Student")),
+                  ElevatedButton(
+                    onPressed: () {
+                      service.deleteStudent("ucbs", rollNoController.text);
+                    },
+                    child: Text("Delete Student"),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
--- lib/Screens/ProfileGate.dart
+++ lib/Screens/ProfileGate.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -8,36 +9,79 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text("Profile Screen"),
-      ),
+      appBar: AppBar(centerTitle: true, title: Text("Profile Screen")),
       body: Padding(
         padding: const EdgeInsets.all(28.0),
-        child: SingleChildScrollView(
-          child:Container(
-            padding: EdgeInsets.all(10),
-              // height: 100,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.circular(12)
-              ),
-              child: Column(
-                children: [
-                  Text("Shaurya Thakur"),
-                  Text("Himachal Pradesh University"),
-                  Text("BCA"),
-                  Text("Computer Science"),
-                  Text("2nd Semester"),
-                ],
-              ),
-            )
-          
+        child: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance.collection("ucbs").snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            }
+
+            if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+              return const Center(child: Text("No Students Found"));
+            }
+
+            final studentDocs = snapshot.data!.docs;
+
+            return ListView.builder(
+              itemCount: studentDocs.length,
+              itemBuilder: (context, index) {
+                return ContainerWidget(
+                  name: studentDocs[index]['name'],
+                  college: studentDocs[index]['college'],
+                  branch: studentDocs[index]['branch'],
+                  rollNo: studentDocs[index]['rollNo'],
+                  semester: studentDocs[index]['semester'],
+                );
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class ContainerWidget extends StatelessWidget {
+  final String name;
+  final String college;
+  final String branch;
+  final int rollNo;
+  final int semester;
+  const ContainerWidget({
+    super.key,
+    required this.name,
+    required this.college,
+    required this.branch,
+    required this.rollNo,
+    required this.semester,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10.0),
+      child: Container(
+        padding: EdgeInsets.all(10),
+        // height: 100,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.grey,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Column(
+          children: [
+            Text(name),
+            Text(college),
+            Text(semester.toString()),
+            Text(branch),
+            Text(rollNo.toString()),
+          ],
         ),
       ),
     );
