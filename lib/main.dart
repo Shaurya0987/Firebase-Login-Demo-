8dcf3e76e36fe2ab1ee1eda88a12c265dc65b0ef
Shaurya Thakur <176936155+Shaurya0987@users.noreply.github.com>
2026-01-25 08:42:36 +0530

    FireBase + Supabase Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebaselogindemo/Screens/UploadScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
